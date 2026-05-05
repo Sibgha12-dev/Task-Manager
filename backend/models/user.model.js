64f5af9b828c9cf9684952b3c2bef94c049fc639
Sibgha Maryam <ullahhadayat526@gmail.com>
2026-05-06 01:35:19 +0500

    Creating user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    profileImageUrl: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFCzxivJXCZk0Kk8HsHujTO3Olx0ngytPrWw&s",
+    },
+
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+},
+    { timestamps: true }
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
