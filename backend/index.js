19a94e8a35768380b1f63d467473362a63ed9ace
Sibgha Maryam <ullahhadayat526@gmail.com>
2026-05-06 01:11:36 +0500

    Connection to Database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,29 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Database is connected");
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express()
 
+//Middleware to handle cors
+app.use(cors({
+    origin: process.env.FRONT_END_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization",],
+
+})
+)
+
+//Middleware to handle JSON in req body
+app.use(express.json())
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 
