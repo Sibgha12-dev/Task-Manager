67ab63db6eec084bfdcac3006b2ffbc76b4ebf9c
Sibgha Maryam <ullahhadayat526@gmail.com>
2026-05-07 00:06:58 +0500

    Middleware to handle possible errors

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,43 +1,43 @@
-import User from "../models/user.model.js"
-import bcryptjs from "bcryptjs"
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res,) => {
-    const { name, email, password, profileImageUrl, adminJoinCode } = req.body
+export const signup = async (req, res, next) => {
+    const { name, email, password, profileImageUrl, adminJoinCode } = req.body;
 
-    if (!name || !email || !password || name === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "Allfields are required" })
+    // 1. Validation
+    if (!name || !email || !password || name === '' || email === '' || password === '') {
+        return next(errorHandler(400, 'All fields are required'));
     }
-
-    // Check if user already exists
-    const isAlreadyExist = await User.findOne({ email })
-
-    if (isAlreadyExist) {
-        return res.status(400).json({ success: false, message: "User already exists" })
-
-    }
-
-    //  Check User role
-    let role = "user"
-
-    if (adminJoinCode && adminJoinCode === process.env.ADMIN_JOIN_CODE) {
-        role = "admin"
-    }
-
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-
-    const newUser = new User({
-        name,
-        email,
-        password: hashedPassword,
-        profileImageUrl,
-        role,
-    })
 
     try {
-        await newUser.save()
+        // 2. Check if user already exists
+        const isAlreadyExist = await User.findOne({ email });
+        if (isAlreadyExist) {
+            return next(errorHandler(400, 'User already exists'));
+        }
 
-        res.json("Signup successful")
+        // 3. Determine User role
+        let role = 'user';
+        if (adminJoinCode && adminJoinCode === process.env.ADMIN_JOIN_CODE) {
+            role = 'admin';
+        }
+
+        // 4. Hash password and create user
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+            profileImageUrl,
+            role,
+        });
+
+        await newUser.save();
+        res.status(201).json('Signup successful');
+
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        // Pass the actual error object to your middleware
+        next(error);
     }
-}
+};
